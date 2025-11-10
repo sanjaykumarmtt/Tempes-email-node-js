5f6638b0a7248dacd35598d1662c01483e95a0fa
sanjaykumarmtt <147645426+sanjaykumarmtt@users.noreply.github.com>
2025-11-10 13:37:34 +0530

    Fix: Ensure app.listen runs directly to fix PORT timeout error

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(cors());
 
 app.use(bodyParser.json());
 dotenv.config();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 
 app.listen();
 // function states_server() {
